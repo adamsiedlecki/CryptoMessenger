54cfeba8e1d5dd246f9013bf4333410d397ee021
adamsiedlecky <adamsiedlec@wp.pl>
2019-08-20 00:03:53 +0200

    Added some classed to git

---
+++ src/main/java/pl/adamsiedlecki/CryptoMessenger/config/Config.java
@@ -0,0 +1,10 @@
+package pl.adamsiedlecki.CryptoMessenger.config;
+
+public class Config {
+
+    private static String imagePath ="src/main/uploads/" ;
+
+    public static String getImagePath() {
+        return imagePath;
+    }
+}
---
+++ src/main/java/pl/adamsiedlecki/CryptoMessenger/fileOperations/ImageConverter.java
@@ -0,0 +1,32 @@
+package pl.adamsiedlecki.CryptoMessenger.fileOperations;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ImageConverter {
+
+    // Source: https://www.javagists.com/convert-an-image-to-base64-string-in-java
+    public static String encodeFileToBase64Binary(File file) throws Exception{
+        FileInputStream fileInputStreamReader = new FileInputStream(file);
+        byte[] bytes = new byte[(int)file.length()];
+        fileInputStreamReader.read(bytes);
+        return new String(Base64.encodeBase64(bytes), "UTF-8");
+    }
+
+    public static File decodeBase64BinaryToFile(String base64Binary, String pathForFile) {
+        byte[] bytes = Base64.decodeBase64(base64Binary);
+        File file;
+        try {
+            FileUtils.writeByteArrayToFile(file = new File(pathForFile), bytes);
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new File("encryption.jpg");
+    }
+
+
+}
